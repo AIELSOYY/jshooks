d685f4328c2025d91a55a0367a4d1d63de590090
AIELSOYY <alexandre.herisse21@gmail.com>
2024-04-22 12:15:50 +0200

    added conditions

--- src/components/ClassCount.js
+++ src/components/ClassCount.js
@@ -3,26 +3,35 @@ import React, { Component } from 'react'
 
 
 class ClassCount extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            count: 0,
+            name: ''
+        }
+    }
+
     componentDidMount(){
         document.title= `Vous avez cliqué ${this.state.count} fois`;
     }
 
-    componentDidUpdate(){
-        document.title= `Vous avez cliqué ${this.state.count} fois`;
-    }
-
-    constructor(props){
-        super(props)
-
-        this.state ={
-            count:0
+    componentDidUpdate(prevProps, prevState){
+        if(this.state.count !== prevState.count){
+            console.log('Mise à jour du titre')
+            document.title= `Vous avez cliqué ${this.state.count} fois`;
         }
     }
+
     render() {
         return(
         <div>
             <h1>{this.state.count}</h1>
             <button onClick={() =>(this.setState({count: this.state.count+1}))}>Add 1 to counter</button>
+            <input type="text" value={this.state.name} onChange={e=> { this.setState({
+                    name: e.target.value
+                })
+            }}/>
         </div>)
         
         
--- src/components/FunctionCount.js
+++ src/components/FunctionCount.js
@@ -3,16 +3,17 @@ import React, {useEffect, useState} from 'react'
 function FunctionCount() {
     const [counter, setCount] = useState(0)
 
+    const [name, setName] = useState('')
     useEffect( () => {
-        setTimeout(() => {
-            document.title= `Vous avez cliqué ${counter} fois`;
-        }, 5000)
-    })
+      console.log('Mise à jour du tire via useEffect')
+      document.title= `Vous avez cliqué ${counter} fois`;
+    }, [counter])
 
   return (
     <div>
-        <p>Vous avez cliqué {counter} fois</p>
-        <button onClick={() =>setCount(counter +1)}>Add 1 to counter</button>
+      <h1>{counter}</h1>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() =>setCount(counter +1)}>Add 1 to counter</button>
     </div>
   )
 }
